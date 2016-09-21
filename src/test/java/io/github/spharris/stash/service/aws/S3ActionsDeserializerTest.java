d3999fd54135bbe2f136156c0af393b81b5840ec
Scott Harris <spharris@gmail.com>
2016-09-21 16:39:52 -0700

    Add files

---
+++ src/test/java/io/github/spharris/stash/service/aws/S3ActionsDeserializerTest.java
@@ -0,0 +1,39 @@
+package io.github.spharris.stash.service.aws;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.amazonaws.auth.policy.actions.S3Actions;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+@RunWith(JUnit4.class)
+public class S3ActionsDeserializerTest {
+
+  final ObjectMapper mapper = new ObjectMapper()
+      .registerModule(new SimpleModule().addDeserializer(
+        S3Actions.class, new S3ActionsDeserializer()));
+  
+  @Test
+  public void deserializesAll() throws Exception {
+    String action = "\"s3:*\"";
+    
+    S3Actions result = mapper.readValue(action, S3Actions.class);
+    S3Actions expected = S3Actions.AllS3Actions;
+    
+    assertThat(result).isEqualTo(expected);
+  }
+  
+  @Test
+  public void deserializesProperty() throws Exception {
+    String action = "\"s3:GetObject\"";
+    
+    S3Actions result = mapper.readValue(action, S3Actions.class);
+    S3Actions expected = S3Actions.GetObject;
+    
+    assertThat(result).isEqualTo(expected);
+  }
+}
---
+++ src/test/java/io/github/spharris/stash/service/aws/StatementTest.java
@@ -0,0 +1,8 @@
+package io.github.spharris.stash.service.aws;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class StatementTest {
+}
