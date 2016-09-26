16df63a6c2b36c30049260aaadd9643f55f2e2f8
Scott Harris <spharris@gmail.com>
2016-09-25 21:15:19 -0700

    Add some DAOs to start transition to sqlite

---
+++ src/main/java/io/github/spharris/stash/service/db/EnvironmentDao.java
@@ -0,0 +1,19 @@
+package io.github.spharris.stash.service.db;
+
+import com.google.common.collect.ImmutableList;
+
+import io.github.spharris.stash.Environment;
+import io.github.spharris.stash.service.request.CreateEnvironmentRequest;
+import io.github.spharris.stash.service.request.DeleteEnvironmentRequest;
+import io.github.spharris.stash.service.request.GetEnvironmentRequest;
+import io.github.spharris.stash.service.request.ListEnvironmentsRequest;
+import io.github.spharris.stash.service.request.UpdateEnvironmentRequest;
+
+public interface EnvironmentDao {
+
+  ImmutableList<Environment> listEnvironments(ListEnvironmentsRequest request);
+  Environment createEnvironment(CreateEnvironmentRequest request);
+  Environment getEnvironment(GetEnvironmentRequest request);
+  Environment updateEnvironment(UpdateEnvironmentRequest request);
+  void deleteEnvironment(DeleteEnvironmentRequest request);
+}
