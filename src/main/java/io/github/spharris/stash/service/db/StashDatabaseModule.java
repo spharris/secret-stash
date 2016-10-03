d9204a2480f65154de98ca51d32e131553d50723
Scott Harris <spharris@gmail.com>
2016-10-02 22:52:59 -0700

    Create the database in an appropriate location

--- src/main/java/io/github/spharris/stash/service/db/StashDatabaseModule.java
+++ src/main/java/io/github/spharris/stash/service/db/StashDatabaseModule.java
@@ -1,5 +1,10 @@
 package io.github.spharris.stash.service.db;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.Key;
 
@@ -9,7 +14,16 @@ public class StashDatabaseModule extends AbstractModule {
 
   @Override
   protected void configure() {
-    bind(Key.get(String.class, DatabaseFile.class)).toInstance("stash-db.sqlite");
+    // Create the database in the same directory as the JAR file
+    try {
+      URI parentDirectory = new File(
+        getClass().getProtectionDomain().getCodeSource().getLocation().toURI())
+          .getParentFile().toURI();
+      bind(Key.get(String.class, DatabaseFile.class)).toInstance(
+        Paths.get(parentDirectory.resolve("stash-db.sqlite")).toAbsolutePath().toString());
+    } catch (URISyntaxException e) {
+      throw new RuntimeException("Unable to open database file", e);
+    }
     
     bind(DatabaseService.class).to(DatabaseServiceImpl.class);
     bind(ProjectDao.class).to(ProjectDaoImpl.class);
